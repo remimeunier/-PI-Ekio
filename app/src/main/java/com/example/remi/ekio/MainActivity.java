8c8808c67501ca3d437ef00482279dc13c407434
Rémi <meunier.remi.p@gmail.com>
2016-05-24 18:21:29 +0200

    displaying menu without icon

--- app/src/main/java/com/example/remi/ekio/MainActivity.java
+++ app/src/main/java/com/example/remi/ekio/MainActivity.java
@@ -2,14 +2,30 @@ package com.example.remi.ekio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] mPlanetTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //test
+        mPlanetTitles = getResources().getStringArray(R.array.planets_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.menu_item, R.id.Itemname, mPlanetTitles));
+        // Set the list's click listener
+        //mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
     }
 }
