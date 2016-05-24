15587446f97dbfaa1812920f6f95203152176cbb
Rémi <meunier.remi.p@gmail.com>
2016-05-24 19:00:37 +0200

    displaying menu with icon

--- app/src/main/java/com/example/remi/ekio/MainActivity.java
+++ app/src/main/java/com/example/remi/ekio/MainActivity.java
@@ -8,22 +8,35 @@ import android.widget.ArrayAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String[] mPlanetTitles;
+    private String[] mMenuItem;
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
+
+    Integer[] imgid={
+            R.drawable.ic_search,
+            R.drawable.ic_settings_applications,
+            R.drawable.ic_settings_applications,
+            R.drawable.ic_settings_applications,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mPlanetTitles = getResources().getStringArray(R.array.planets_array);
+        //recup la list des menu item
+        mMenuItem = getResources().getStringArray(R.array.menu_item);
+
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
+
         // Set the adapter for the list view
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
-                R.layout.menu_item, R.id.Itemname, mPlanetTitles));
+       // mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.menu_item, R.id.Itemname, mMenuItem));
+        // custom adapater
+        CustomListAdapter adapter=new CustomListAdapter(this, mMenuItem, imgid);
+        mDrawerList.setAdapter(adapter);
+
         // Set the list's click listener
         //mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
