7563b2648ab1244e9136a7c4a4c39b508e7e3d87
ekioawesome <thugrac@gmail.com>
2016-06-15 13:48:32 +0200

    petits trucs

---
+++ app/src/main/java/com/example/remi/ekio/SettingActivity.java
@@ -0,0 +1,15 @@
+package com.example.remi.ekio;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Hoang Nam on 15/06/2016.
+ */
+public class SettingActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
